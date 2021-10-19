0c27a04efc1028b1214ad9bf8d35f50c39eeb6ea
Fernando Barrientos <linofernando2703@gmail.com>
2021-10-18 23:32:43 -0400

    Arregle el bug que rompia el codigo cuando scrapeaba datos sin un patron

--- clases_transfermarkt/clase_scraping_transfermarkt.py
+++ clases_transfermarkt/clase_scraping_transfermarkt.py
@@ -6,198 +6,197 @@ class scraping_transfermarkt:
     def __init__(self):
         pass
     def scraper_jugadores(self,url):
-
+        """
+        Aún me falta agregar el contenido de las redes sociales, banderas y demas iconos,  
+        también datos de la segunda y tercera tabla. Esto solo es una demostracion para la 
+        PRIMERA TABLA
+        Se aceptan consejos
+        """
         keys = [
-            # PRIMERA TABLA
-            'Name',
-            'Date of Birth',
-            'Place of Birth',
-            'Age',
-            'Height',
-            'Citizenship',
-            'Position',
-            'Foot',
-            'Player Agent',
-            'Current Club',
-            'Joined',
-            'Contract Expired',
-            'Contract Option',
-            'Outfitter',
-            'Social Media',
-            'Social Media Icons',
-            'Flags Icons',
-            # SEGUNDA TABLA
-            'Current Club Link',
-            'League',
-            'League Level',
-            # TERCERA TABLA
-            'Market Value',
-            'Last Update Market Value'
+                # PRIMERA TABLA
+                'Name in home country',
+                'Date of birth',
+                'Place of birth',
+                'Age',
+                'Height',
+                'Citizenship',
+                'Position',
+                'Foot',
+                'Player agent',
+                'Current club',
+                'Joined',
+                'Contract option',
+                'Contract expires',
+                'Outfitter',
+                'Social Media',
+                'Social Media Icons',
+                'Flags Icons',
+                # SEGUNDA TABLA
+                'Current Club Link',
+                'League',
+                'League Level',
+                # TERCERA TABLA
+                'Market Value', 
+                'Last Update Market Value'
         ]
-        # Creando el diccionario
+        # Creo mi diccionario
         player_info = dict.fromkeys(keys)
 
         headers = {'User-Agent': 'placeholder'}
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.content, 'lxml')
 
-        """ 
-        PRIMERA TABLA  
-
-        De esta tabla obtendremos estos datos: 
-
-        data =  [
-                'Nombre', 'Fecha de Nacimiento', 'Lugar de Nacimiento', 'Edad',
-                'Altura', 'Nacionalidad', 'Posición', 'Pie dominante', 'Agente',
-                'Club Actual', 'Inicio de Contrato', 'Finalización de su Contrato', 
-                'Patrocinador', 'Redes Sociales'
-                ]
-           
-        """
-        # Almacenara todos los valores de data_player
-        values_list = []
-
-        # PRIMERA TABLA de los datos del jugador
+        # PRIMERA TABLA
         first_table = soup.find('div', class_='large-6 large-pull-6 small-12 columns spielerdatenundfakten')
+        # KEYS de la PRIMERA TABLA 
+        data_regular = first_table.find_all('span', class_='info-table__content info-table__content--regular')
+        # VALUES de la PRIMERA TABLA 
+        data_bold = first_table.find_all('span', class_='info-table__content info-table__content--bold')
 
-        # En data_player almacenamos todos los dataValue de los datos del jugador de la tabla principal
-        data_player = first_table.find_all('span', class_='info-table__content info-table__content--bold')
+        # En algunas ocasiones no se encuentra el nombre del jugador en la primera tabla
+        # por eso lo saco de otra seccion.
+        player_name = soup.find('div', class_='dataName').h1
+        player_info['Name in home country'] = player_name.text.strip()
 
-        # Iteramos sobre la primera tabla y agregamos los valores a values_list
-        # FIXME: Aca surge la duda ya que los datos de la primera tabla no tienen un patron 
-        # en especifico, en algunos casos tienen los 15 o 14 datos en otros solo tienen 4 o 9
-        # Como desarrollar un patron o metodo que se encarge de esto
-        try:
-            for info in data_player:
-                # Esto elimina los \xa0 del código
-                ordered_info = info.text.strip().replace('\xa0', ' ')
-                values_list.append(ordered_info)
+        # Lista con las llaves de la primera table
+        data_regs = []
+        # Lista con los valores de la primera tabla
+        data_bolds = []
+        for info in data_bold:
+            info = info.text.strip().replace('\xa0', ' ')
+            data_bolds.append(info)
 
-            name = values_list[0]
-            player_info['Name'] = name
+        for idx, data_reg in enumerate(data_regular):
+            data_reg = data_reg.text.strip().replace(':', '')
+            data_regs.append(data_reg)
+            # Compruebo si un dato esta en la tabla iterando sobre las llaves de la primera tabla
+            # si es así se agrega el dato, sino continua buscando, si no lo encuentra se queda en None
+            try:
+                if 'Date of birth' in data_regs[idx]:
+                    # Si es su cumpleaños omitimos regresar la palabra Happy Birthday
+                    player_info['Date of birth'] = data_bolds[idx].replace(' Happy Birthday', '')
 
-            birth_date = values_list[1]
-            player_info['Date of Birth'] = birth_date
+                elif 'Place of birth' in data_regs[idx]:
+                    player_info['Place of birth'] = data_bolds[idx]
 
-            place_of_birth = values_list[2]
-            player_info['Place of Birth'] = place_of_birth
+                elif 'Age' in data_regs[idx]:
+                    player_info['Age'] = int(data_bolds[idx])
 
-            age = int(values_list[3])
-            player_info['Age'] = age
+                elif 'Height' in data_regs[idx]:
+                    player_info['Height'] = float(data_bolds[idx].replace(',', '.').replace(' m', ''))
 
-            height = values_list[4].replace(',', '.')
-            player_info['Height'] = float(height.replace(' m', ''))
+                elif 'Citizenship' in data_regs[idx]:
+                    player_info['Citizenship'] = data_bolds[idx].split()
 
-            citizenship = values_list[5].split() 
-            player_info['Citizenship'] = citizenship
+                elif 'Position' in data_regs[idx]:
+                    player_info['Position'] = data_bolds[idx]
 
-            position = values_list[6]
-            player_info['Position'] = position
+                elif 'Foot' in data_regs[idx]:
+                    player_info['Foot'] = data_bolds[idx]
 
-            dominant_foot = values_list[7]
-            player_info['Foot'] = dominant_foot
+                elif 'Player agent' in data_regs[idx]:
+                    player_info['Player agent'] = data_bolds[idx]
+                    
+                elif 'Current club' in data_regs[idx]:
+                    player_info['Current club'] = data_bolds[idx] 
 
-            player_agent = values_list[8]
-            player_info['Player Agent'] = player_agent
+                elif 'Joined' in data_regs[idx]:
+                    player_info['Joined'] = data_bolds[idx]
 
-            current_club = values_list[9]
-            player_info['Current Club'] = current_club
+                elif 'Contract expires' in data_regs[idx]:
+                    player_info['Contract expires'] = None if data_bolds[idx] == '-' else data_bolds[idx]
 
-            commencement_date = values_list[10]
-            player_info['Joined'] = commencement_date 
+                elif 'Contract option' in data_regs[idx]:
+                    player_info['Contract option'] = None if data_bolds[idx] == '-' else data_bolds[idx]  
 
-            contract_expired = values_list[11]
-            player_info['Contract Expired'] = contract_expired
-            # NOTA: Esto del if lo aplique porque pense que solo el contract option era el que variaba
-            # Si el len de lista es 15 significa que tiene contract_option
-            if len(values_list) == 15:
-                contract_option = values_list[12]
-                player_info['Contract Option'] = contract_option
+                elif 'Outfitter' in data_regs[idx]:
+                    player_info['Outfitter'] = None if data_bolds[idx] == '-' else data_bolds[idx]
 
-                outfitter = values_list[13]
-                player_info['Outfitter'] = outfitter if outfitter else None
-            # Si el len de lista es 14 significa que no tiene contract_option
-            else:
-                outfitter = values_list[12]
-                player_info['Outfitter'] = outfitter if outfitter else None
-
-        except IndexError:
-            print('El indice a buscar no existe')
-
-
+            except IndexError:
+                print('Error con el tope del indice')
+            
+             
         # LINKS DE LA PRIMERA TABLA
+        # Url de las redes sociales
         links_list = []
+        # Url de los iconos de las redes sociales
         link_images_list = []
         # El div que contiene las redes sociales
-        social_media_links = first_table.find('div', class_='socialmedia-icons')
-    
-        for link in social_media_links.find_all('a'):
-            # Url de las redes sociales
-            links_list.append(link['href'])
-            # Url de los iconos de las redes sociales
-            link_images_list.append(link.img['data-src'])
-    
-        player_info['Social Media'] = links_list
-        player_info['Social Media Icons'] = link_images_list
-
-        # Banderas de la primera Tabla
-        flags = []
-        url_flags = first_table.find_all('img', class_='flaggenrahmen')
+        try:
+            social_media_links = first_table.find('div', class_='socialmedia-icons')
+            
+            for link in social_media_links.find_all('a'):
+                links_list.append(link['href'])
+                link_images_list.append(link.img['data-src'])
         
-        for flag in url_flags:
-            # Filtra las imagenes de los gifts
-            if 'https://' in flag['src']:
-                flags.append(flag['src'])
+            player_info['Social Media'] = links_list
+            player_info['Social Media Icons'] = link_images_list
+        # Si no se encuentra el div con las redes sociales se quedan en None
+        except:
+            print('No se encontro la etiqueta div "socialmedia-icons" que contienen las redes sociales')
         
-        player_info['Flags Icons'] = flags
-        """ 
-        SEGUNDA TABLA 
-        De esta tabla obtendremos estos datos:
-        data = [Current Club Link', 'League', 'League Level']
-        """
+        # BANDERAS DE LA PRIMERA TABLA
+        try:
+            flags = []
+            url_flags = first_table.find_all('img', class_='flaggenrahmen')
+            
+            for flag in url_flags:
+                # Filtra las imagenes de los gifts
+                if 'https://' in flag['src']:
+                    flags.append(flag['src'])
+            player_info['Flags Icons'] = flags
+        except:
+            # Si no existen los datos se quedan en None
+            print('No se encontro la etiqueta img "flaggenrahmen" que contiene las banderas')
 
-        # SEGUNDA TABLA con los datos del jugador
+
+
+        # SEGUNDA TABLA
         second_table = soup.find('div', class_='dataZusatzDaten')
-        league_and_team = second_table.find_all('span')
+        try:
+            current_link = second_table.find('span', class_='hauptpunkt')
+            link = current_link.a['href'] if 'Retired' not in current_link.text else None
+            # Completamos la url incompleta que nos da la página
+            url = ['https://www.transfermarkt.com', link]
+            player_info['Current Club Link'] = ''.join(url)
 
-        # Completamos la url incompleta que nos da la página
-        link = second_table.find('a')['href']
-        url = ['https://www.transfermarkt.com', link]
-        player_info['Current Club Link'] = ''.join(url)
+            league = second_table.find('span', class_='mediumpunkt')
+            player_info['League'] = league.text.strip()
 
-        player_info['League'] = league_and_team[1].text.strip()
-        player_info['League Level'] = league_and_team[3].text.strip()
+            data_value = second_table.find('span', class_='dataValue')
+            data_item = second_table.find('span', class_='dataItem')
+            player_info['League Level'] = data_value.text.strip() if 'League level:' in data_item.text else None
+
+        except:
+            # Si no existen los datos se quedan en None
+            print('Algunos datos de la segunda tabla podrian estar vacios o inexistentes')
+        
 
 
-        """ 
-        TERCERA TABLA 
-        De esta tabla obtendremos estos datos:
-        data = ['market_value', 'last_update_market_value']
-        """
-
-        # TERCERA TABLA con los datos del jugador
+        # TERCERA TABLA
         fourth_table = soup.find('div', class_='large-5 columns small-12')
-        # Datos de los valores de mercado 
-        market_values = fourth_table.find_all('div', class_='right-td')
+        # Datos de los valores de mercado
+        try: 
+            market_values = fourth_table.find_all('div', class_='right-td')
+            market_value = market_values[0].text.strip()
+            player_info['Market Value'] = None if market_value == '-' else market_value
+            player_info['Last Update Market Value'] = market_values[1].text.strip()
+        except:
+            # Si no existen los datos se quedan en None
+            print('Algunos datos de la tercera tabla podrian estar vacios o inexistentes')
 
-        player_info['Market Value'] = market_values[0].text.strip()
-        player_info['Last Update Market Value'] = market_values[1].text.strip()
 
         return player_info
 
-    def scraper_transferencias(url):
-        pass
-    def scraper_empresas(url):
-        pass
-
 if __name__ == '__main__':
+    #url = 'https://www.transfermarkt.com/pape-cheikh/profil/spieler/336828'
     #url = 'https://www.transfermarkt.com/lionel-messi/profil/spieler/28003'
-    #url = 'https://www.transfermarkt.com/kylian-mbappe/profil/spieler/342229'
-    #url = 'https://www.transfermarkt.com/junior-dina-ebimbe/profil/spieler/536482'
-    url = 'https://www.transfermarkt.com/pape-cheikh/profil/spieler/336828'
-    
+    #url = 'https://www.transfermarkt.com/zakaria-messibah/profil/spieler/416107'
+    #url = 'https://www.transfermarkt.com/fabio-messi/profil/spieler/673358'
+    #url = 'https://www.transfermarkt.com/jiri-krejci/profil/spieler/25595'
+    #url = 'https://www.transfermarkt.com/javier-chevanton/profil/spieler/6420'
+    url = 'https://www.transfermarkt.com/javier-zeoli/profil/spieler/258059'
+    #url = 'https://www.transfermarkt.com/bandiougou-fadiga/profil/spieler/590914'
     player = scraping_transfermarkt()
     datos_del_jugador = player.scraper_jugadores(url)
     print(datos_del_jugador)
-    
-
