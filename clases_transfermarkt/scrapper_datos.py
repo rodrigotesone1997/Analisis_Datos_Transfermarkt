70f2ce99fd18970216e692724d5a05805b3b11d5
sstandre <simon@saintandre.com.ar>
2021-11-26 19:02:22 -0300

    adaptar scraper_transferencias a otra pagina y convertir toda la salida a texto

--- clases_transfermarkt/scrapper_datos.py
+++ clases_transfermarkt/scrapper_datos.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 class scraping:
-    def scraper_jugadores(url):
+    def scraper_jugadores(self, url):
         """
         Aún me falta agregar el contenido de las redes sociales, banderas y demas iconos,  
         también datos de la segunda y tercera tabla. Esto solo es una demostracion para la 
@@ -185,7 +185,7 @@ class scraping:
         return player_info
 
 
-    def scraper_transferencias(url):
+    def scraper_transferencias(self, url):
 
         keys =[
                 'url_imagen',
@@ -197,7 +197,7 @@ class scraping:
                 'nacionalidad_equipo_abandonado',
                 'nacionalidad_equipo_unido',
                 'equipo_unido',
-                'fecha_transferencia',
+                # 'fecha_transferencia',
                 'valor_mercado',
                 'fee',
             ]
@@ -209,83 +209,86 @@ class scraping:
             page = requests.get(url, headers=headers)
             soup = BeautifulSoup(page.content, 'html.parser')
             table = soup.find('table', class_='items')
-
             body = table.find('tbody')
 
-            for row in body.contents:
-                # Ignoramos filas None
-                if not row.name: continue
-    
-                transfer = dict.fromkeys(keys)
-    
-                columns = row.contents
-                #Cada columna tiene un estilo especial, extraemos los datos de a una
-                
-                player = columns[1]
-                tds = player.find_all('td')
-    
-                url_foto = tds[0].find('img')['data-src']
-                transfer['url_imagen'] = url_foto
-    
-                nombre_completo = tds[1].find('a').text
-                transfer['nombre_completo'] = nombre_completo
-
-                posicion = tds[2].text
-                transfer['posicion'] = posicion
-
-                age = columns[2]
-                edad = int(age.text) if age.text.isdigit() else ""
-                transfer['edad'] = edad
-
-                nationality = columns[3]
-                nats = [flag['title'] for flag in nationality.find_all('img')]
-                transfer['nacionalidad'] = nats
-
-                left = columns[4]
-                team_elem = left.find('td', class_='hauptlink')
-                equipo_or = team_elem.find('a').text
-                transfer['equipo_abandonado'] = equipo_or
-
-                # No podemos tomar el pais del texto, ya que en algunos casos es el
-                # nombre de la liga. Lo extraemos de la imagen de la bandera. 
-                # Algunos nombres de paises pueden estar en aleman.
-                pais_or_elem = left.find('img', class_='flaggenrahmen')
-                # Además, cuando el pais de origen o llegada es "Without club", no 
-                # tengo pais
-                pais_or = pais_or_elem['title'] if pais_or_elem else ""
-                transfer['nacionalidad_equipo_abandonado'] = pais_or
-    
-                joined = columns[5]
-                team_elem = joined.find('td', class_='hauptlink')
-                equipo_un_elem = team_elem.find('a')
-                # Ademas de las consideraciones para los paises, cuando el equipo de 
-                # llegada es "Retired", el elemento no es un link, y tengo que contarlo
-                # por separado
-                equipo_un = equipo_un_elem.text if equipo_un_elem else 'Retirado'
-                transfer['equipo_unido'] = equipo_un
-                
-                pais_un_elem = joined.find('img', class_='flaggenrahmen')
-                pais_un = pais_un_elem['title'] if pais_un_elem else ""
-                transfer['nacionalidad_equipo_unido'] = pais_un
-    
-                tr_date = columns[6]
-                transfer['fecha_transferencia'] = tr_date.txt
-
-                mkt_value = columns[7]
-                transfer['valor_mercado'] = mkt_value.text
-
-                fee = columns[8]
-                transfer['fee'] = fee.find('a').text
-                # text, _, money = fee.partition('€')
-                # if money:
-                #     fee = self.convert_currency_float(money)
-                # else:
-                #     fee = text
-                # transfer['fee'] = fee
-
-                transfers_list.append(transfer)
         except AttributeError:
             print("No hay elementos disponibles en este url\n")
+            return
+
+        for row in body.contents:
+            # Ignoramos filas None
+            if not row.name: continue
+
+            transfer = dict.fromkeys(keys)
+
+            columns = row.contents
+
+            #Cada columna tiene un estilo especial, extraemos los datos de a una
+            
+            player = columns[2]
+            tds = player.find_all('td')
+
+            url_foto = tds[0].find('img')['data-src']
+            transfer['url_imagen'] = url_foto
+
+            nombre_completo = tds[1].find('a').text
+            transfer['nombre_completo'] = nombre_completo
+
+            posicion = tds[2].text
+            transfer['posicion'] = posicion
+
+            age = columns[3]
+            # edad = int(age.text) if age.text.isdigit() else ""
+            transfer['edad'] = age.text   # queremos texto a la salida
+
+            mkt_value = columns[4]
+            transfer['valor_mercado'] = mkt_value.text
+
+            nationality = columns[5]
+            nats = [flag['title'] for flag in nationality.find_all('img')]
+            transfer['nacionalidad'] = ','.join(nats)
+
+            left = columns[6]
+            team_elem = left.find('td', class_='hauptlink')
+            equipo_or = team_elem.find('a').text
+            transfer['equipo_abandonado'] = equipo_or
+
+            # No podemos tomar el pais del texto, ya que en algunos casos es el
+            # nombre de la liga. Lo extraemos de la imagen de la bandera. 
+            # Algunos nombres de paises pueden estar en aleman.
+            pais_or_elem = left.find('img', class_='flaggenrahmen')
+            # Además, cuando el pais de origen o llegada es "Without club", no 
+            # tengo pais
+            pais_or = pais_or_elem['title'] if pais_or_elem else ""
+            transfer['nacionalidad_equipo_abandonado'] = pais_or
+
+            joined = columns[7]
+            team_elem = joined.find('td', class_='hauptlink')
+            equipo_un_elem = team_elem.find('a')
+            # Ademas de las consideraciones para los paises, cuando el equipo de 
+            # llegada es "Retired", el elemento no es un link, y tengo que contarlo
+            # por separado
+            equipo_un = equipo_un_elem.text if equipo_un_elem else 'Retirado'
+            transfer['equipo_unido'] = equipo_un
+            
+            pais_un_elem = joined.find('img', class_='flaggenrahmen')
+            pais_un = pais_un_elem['title'] if pais_un_elem else ""
+            transfer['nacionalidad_equipo_unido'] = pais_un
+
+            # tr_date = columns[6]
+            # transfer['fecha_transferencia'] = tr_date.txt                
+
+            fee = columns[8]
+            transfer['fee'] = fee.text
+            # text, _, money = fee.partition('€')
+            # if money:
+            #     fee = self.convert_currency_float(money)
+            # else:
+            #     fee = text
+            # transfer['fee'] = fee
+
+            transfers_list.append(transfer)
+
         return transfers_list
 
     # def convert_currency_float(self, text):
@@ -303,3 +306,10 @@ class scraping:
 
     def scraper_empresas(url):
         pass
+
+
+url = r"https://www.transfermarkt.com/transfers/saisontransfers/statistik?land_id=0&ausrichtung=&spielerposition_id=&altersklasse=&leihe=&transferfenster=&saison-id=0&plus=1"
+scraper = scraping()
+transfers = scraper.scraper_transferencias(url)
+from pprint import pprint
+pprint(transfers)
