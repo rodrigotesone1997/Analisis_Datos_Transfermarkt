a6c73941d0851e9f3659edfd0ac3b66959e2eba1
sstandre <simon@saintandre.com.ar>
2021-12-03 16:19:33 -0300

    creacion de la funcion

--- clases_transfermarkt/recoleccion_datos.py
+++ clases_transfermarkt/recoleccion_datos.py
@@ -1,2 +1,29 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+from scrapper_datos import scraping
+
+
+URL_TRANSFERENCIAS =r"https://www.transfermarkt.com/transfers/saisontransfers/statistik?plus=1"
+
 class recoleccion_datos:
-    pass
+
+    def __init__(self):
+
+        self.scraper = scraping()
+
+        opts = Options()
+        opts.headless = False  
+        self.browser = Chrome(options=opts)
+    
+    def recolectar_transferencias(self):
+        
+        self.browser.get(URL_TRANSFERENCIAS)
+        select = Select(self.browser.find_element(By.ID, "selUJM"))
+        select.select_by_visible_text('19/20 ')
+
+
+rd = recoleccion_datos()
+rd.recolectar_transferencias()
